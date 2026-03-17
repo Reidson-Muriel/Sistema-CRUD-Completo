3a0d8c53a299970b41b975568e7b8ffa4efe1582
Reidson-Muriel <reidsonmuriel497@gmail.com>
2026-03-16 21:38:44 -0300

    ajust template for deploy

--- sistema/app.py
+++ sistema/app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from sistema.routes.contatos_routes import contato_bp
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../frontend")
 CORS(app)
 app.register_blueprint(contato_bp)
 
 @app.route("/")
 def home():
-    return "Sistema CRUD rodando online 🚀"
+    return render_template("index.html")
     
 ## rota para exercutar no dispositivo moveis
 if __name__ == "__main__":
